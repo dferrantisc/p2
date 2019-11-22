37c133b8fca6757cc6fe8f2e78b7270db3f52099
dferranti <dferrantisc@hotmail.com>
2019-11-22 11:43:09 -0400

    Add o crud

--- admin.php
+++ admin.php
@@ -1,77 +1,37 @@
 <?php
+	if($_SERVER["REQUEST_METHOD"]=="POST"){
+		include("connection.php");
 
-    $masqli = new mysqli('host','root','pass','p2') or die(mysqli_error($mysqli));
+		$tipo = $_POST["tipo"]; //insert, update, delete
+		$sql = "";
 
-    if(){
+		switch ($tipo) {
+			case 'insert':
+				$nomevend = $_POST["nomevend"];
+				$salario = $_POST["salario"];
+				$codsetor = $_POST["codsetor"];
+				$sql = "INSERT INTO `vendedor`(`NOMEVEND`, `SALARIO`, `CODSETOR`) VALUES ('$nomevend','$salario',$codsetor)";
+				break;
+			case 'update':
+				$id = $_POST["id"];
+				$nomevend = $_POST["nomevend"];
+				$salario = $_POST["salario"];
+				$codsetor = $_POST["codsetor"];
+				$sql = "UPDATE `vendedor` SET `NOMEVEND`='$nomevend',`SALARIO`='$salario',`CODSETOR`=$codsetor WHERE `CODVEND`=$id";
+				break;
+			case 'delete':
+				$id = $_POST["id"];
+				$sql = "DELETE FROM `vendedor` WHERE `CODVEND`=$id";
+				break;
+		}
 
-    }else{
-        
-    }
+		echo $sql;
 
+		if($sql){
+			$result = $conn->query($sql);
+		}
 
+	}else{
+		header("Location: index.php");
+	}
 ?>
-
-<?php
-
-    require("conec.php");
-
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>admin</title>
-    </head>
-    
-    <body>
-
-    <h2>Controle</h2>
-            <table> 
-            <tr>
-                <th>Nome de Usuários</th>
-                    <th>Senha dos Usuários</th>
-            
-            </tr>
-
-    <?php
-
-        $sql = "SELECT * FROM login";
-
-        $result= $bd->query($sql);
-        echo "<pre/>";
-        var_dump($result->fetch_all()[0][1]);
-        echo "<pre/>";
-
-        $ruser= $result->fetch_all();
-
-        for($i=0;$i<10;$i++)
-        {
-        
-            echo "das";
-       
-    ?>
-
-            <tr>
-                <td><?php echo $ruser[$i][1]; ?></td>
-                <td><?php echo $ruser[$i][2]; ?></td>
-                        
-            </tr>
-
-
-
-
-    <?php
-         }
-    ?>
-
-            
-            </table>        
-        
-    </body>
-
-</html>
